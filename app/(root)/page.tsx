1bd5ea81667b599f4f97fa298c08c39ded6cac5e
myladd <mylad.zandkarimi@gmail.com>
2024-08-18 14:31:54 +0330

    implement right sidebar and complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Milad" };
+  const loggedIn = { firstName: "Milad", lastName: "ZK", email: "milo@gmail.com" };
   return (
     <section className="home">
       <div className="home-content">
@@ -13,6 +14,7 @@ const Home = () => {
         </header>
         RECENT TRANSACTIONS
       </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
     </section>
   );
 };
